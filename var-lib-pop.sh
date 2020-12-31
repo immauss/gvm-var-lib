b63ae1cd1c52ced4e2c4ffbfb4866a3bc25c357d
GE Scott Knauss <scott@OldMac.local>
2020-12-31 17:15:58 +0100

    typos

--- var-lib-pop.sh
+++ var-lib-pop.sh
@@ -9,23 +9,25 @@
 # https://github.com/immauss/openvas
 # YMMV for other setups.
 TARURL="https://github.com/immauss/gvm-var-lib/raw/main/var-lib.tar.xz"
-if [ ! -x "$1" ]; then
+if [ -x "$1" ]; then
 	VOLUME="openvas"
 else
 	VOLUME="$1"
 fi
 echo -e "\tUsing $VOLUME as the container name. You can change this with:"
 echo -e "\t\t$0 <container-name>\n\n"
-if [ -f ./var-lib.tar.xz ]; then
+echo "checking for archive"
+if [ ! -f var-lib.tar.xz ]; then
 	echo "No archive found. Would you like to download it now?"
 	read junk
 	if [ $junk == "Y" ] || [ $junk == "y" ] ; then
-		curl --url $TARURL --output var-lib.tar.xz
+		curl -L --url $TARURL --output var-lib.tar.xz
 	else
 		echo "OK"
 		exit
 	fi
-docker pull alpine 
-docker run -d -v openvas-test:/data -v $(pwd)/mnt --name copypine alpine sleep 1d
-docker exec -t $VOLUME sh -c "cd /data/; tar xvf /mnt/var-lib.tar.xz " 
+fi
+#docker pull alpine 
+docker run -d -v $VOLUME:/data -v $(pwd):/mnt --name copypine alpine sleep 1d
+docker exec -t copypine sh -c "cd /data/; tar xvf /mnt/var-lib.tar.xz " 
 docker rm -f copypine
